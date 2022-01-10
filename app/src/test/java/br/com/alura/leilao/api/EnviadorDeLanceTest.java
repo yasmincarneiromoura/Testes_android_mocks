c28874395721c76d625e477c1a3bd388c9d61a98
Yasmin Carneiro Moura <ycarneir@emeal.nttdata.com>
2022-01-10 14:09:16 -0300

    commit curso mocks

---
+++ app/src/test/java/br/com/alura/leilao/api/EnviadorDeLanceTest.java
@@ -0,0 +1,61 @@
+package br.com.alura.leilao.api;
+
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import br.com.alura.leilao.api.retrofit.client.LeilaoWebClient;
+import br.com.alura.leilao.exception.LanceMenorQueUltimoLanceException;
+import br.com.alura.leilao.exception.UsuarioJaDeuCincoLancesException;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.ui.dialog.AvisoDialogManager;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnviadorDeLanceTest {
+
+    @Mock
+    private LeilaoWebClient client;
+    @Mock
+    private EnviadorDeLance.LanceProcessadoListener listener;
+    @Mock
+    private AvisoDialogManager manager;
+    @Mock
+    private Leilao leilao;
+
+    @Test
+    public void deve_MostrarMensagemDeFalha_QuandoOLanceForMenorQueUltimoLance(){
+        EnviadorDeLance enviador = new EnviadorDeLance(
+                client,
+                listener,
+                manager);
+        doThrow(LanceMenorQueUltimoLanceException.class).
+                when(leilao).propoe(ArgumentMatchers.any(Lance.class));
+
+        enviador.envia(leilao, new Lance(new Usuario("Fran"), 100));
+
+        verify(manager).mostraAvisoLanceMenorQueUltimoLance();
+    }
+
+    @Test
+    public void deve_MostrarMensagemDeFalha_QuandoUsuarioDerNovoLance(){
+        EnviadorDeLance enviador = new EnviadorDeLance(
+                client,
+                listener,
+                manager);
+        doThrow(UsuarioJaDeuCincoLancesException.class).
+                when(leilao).propoe(ArgumentMatchers.any(Lance.class));
+
+        enviador.envia(leilao, new Lance(new Usuario("Alex"), 200));
+
+        verify(manager).mostraAvisoUsuarioJaDeuCincoLances();
+
+    }
+
+}
